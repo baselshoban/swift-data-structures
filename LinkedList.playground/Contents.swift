766977145dc17901025f31a9fbfd1e0c5b16a219
Basel Shoban <basel.sh@likesyria.sy>
2019-08-15 00:20:02 +0200

    add removing functionality

--- LinkedList.playground/Contents.swift
+++ LinkedList.playground/Contents.swift
@@ -2,8 +2,10 @@ import UIKit
 
 class LinkedList<T> {
     
+    // MARK: - Properties
     var head : Node<T>?
     
+    // MARK: - Adding elements
     func append(element: T) {
         let newNode = Node(data: element)
         
@@ -29,6 +31,31 @@ class LinkedList<T> {
         node.next = newNode
     }
     
+    // MARK: - Deleting elements
+    func popElement() {
+        head = head?.next
+    }
+    
+    func removeElementAt(index: Int) {
+        
+        // prevent invalid indices
+        guard index >= 0 else {return}
+        
+        var nodeHolder = head
+        var counter = 0
+        
+        // check for head removal
+        guard index != 0 else {nodeHolder = nodeHolder?.next; return}
+        
+        while nodeHolder != nil && counter < index - 1 {
+            nodeHolder = nodeHolder?.next
+            counter += 1
+        }
+        
+        nodeHolder?.next = nodeHolder?.next?.next
+    }
+    
+    // MARK: - Show list contents
     func printList() {
         var node = head
         while node != nil {
@@ -36,6 +63,11 @@ class LinkedList<T> {
             node = node!.next
         }
     }
+    
+}
+
+// MARK: - Node Object
+extension LinkedList {
     
     class Node<T> {
         var data : T
@@ -45,13 +77,25 @@ class LinkedList<T> {
             self.data = data
         }
     }
+    
 }
 
 var list = LinkedList<String>()
 
 list.append(element: "The Head")
 list.append(element: "second element")
-list.push(element: "the new Head HAHAH")
-list.append(element: "I am in the middle", after: list.head!.next!)
+//list.push(element: "the new Head HAHAH")
+//list.append(element: "I am in the middle", after: list.head!.next!)
 
+list.printList()
+
+list.removeElementAt(index: -2)
+
+print("\n\nAfter Update:\n")
+list.printList()
+
+list.popElement()
+list.popElement()
+
+print("\n\nAfter Update:\n")
 list.printList()
