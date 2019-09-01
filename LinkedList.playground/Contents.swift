823b6c0304d3810333ff7b9d0cc03d91fb375349
Basel Shoban <basel.sh@likesyria.sy>
2019-09-01 21:47:19 +0200

    Improve Linked List structure

--- LinkedList.playground/Contents.swift
+++ LinkedList.playground/Contents.swift
@@ -5,23 +5,17 @@ class LinkedList<T> {
     // MARK: - Properties
     var head : Node<T>?
     
-    // MARK: - Adding elements
+    // MARK: - Main Actions
     func append(element: T) {
         let newNode = Node(data: element)
         
-        guard var node = head else { head = newNode; return}
+        guard var node = head else { head = newNode; return }
         
         while node.next != nil {
             node = node.next!
         }
         
         node.next = newNode
-    }
-    
-    func push(element: T) {
-        let newNode = Node(data: element)
-        newNode.next = head
-        head = newNode
     }
     
     func append(element: T, after node: Node<T>) {
@@ -31,12 +25,25 @@ class LinkedList<T> {
         node.next = newNode
     }
     
-    // MARK: - Deleting elements
-    func popElement() {
-        head = head?.next
+    func remove() {
+        
+        guard var holder = head else { return; }
+        var previous: Node<T>?
+        
+        while holder.next != nil {
+            previous = holder
+            holder = holder.next!
+        }
+        
+        if previous != nil {
+            previous?.next = nil
+        } else {
+            head = nil
+            
+        }
     }
     
-    func removeElementAt(index: Int) {
+    func removeAt(index: Int) {
         
         // prevent invalid indices
         guard index >= 0 else {return}
@@ -54,6 +61,20 @@ class LinkedList<T> {
         
         nodeHolder?.next = nodeHolder?.next?.next
     }
+
+    // MARK: - Extra Action to support using Linked List in Queue & Stack data structures
+    
+    func push(element: T) {
+        let newNode = Node(data: element)
+        newNode.next = head
+        head = newNode
+    }
+    
+    // MARK: - Deleting elements
+    func pop() {
+        head = head?.next
+    }
+    
     
     // MARK: - Show list contents
     func printList() {
@@ -84,18 +105,18 @@ var list = LinkedList<String>()
 
 list.append(element: "The Head")
 list.append(element: "second element")
-//list.push(element: "the new Head HAHAH")
-//list.append(element: "I am in the middle", after: list.head!.next!)
+list.push(element: "the new Head HAHAH")
+list.append(element: "I am in the middle", after: list.head!.next!)
 
 list.printList()
 
-list.removeElementAt(index: -2)
+list.removeAt(index: 2)
 
 print("\n\nAfter Update:\n")
 list.printList()
 
-list.popElement()
-list.popElement()
+list.pop()
+list.remove()
 
 print("\n\nAfter Update:\n")
 list.printList()
